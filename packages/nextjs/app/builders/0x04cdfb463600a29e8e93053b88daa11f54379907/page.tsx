aaa921badeae92c157133633c7d169f05f0f5964
Koray <128939343+dharmanan@users.noreply.github.com>
2025-12-01 15:45:01 +0000

    fix: Remove 'use client' directive and revert yarn.lock changes

--- packages/nextjs/app/builders/0x04cdfb463600a29e8e93053b88daa11f54379907/page.tsx
+++ packages/nextjs/app/builders/0x04cdfb463600a29e8e93053b88daa11f54379907/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import { DiscordIcon, FarcasterIcon, GithubIcon, SpeedRunIcon, TelegramIcon, XIcon } from "./icons";
 import { Address } from "@scaffold-ui/components";
